07aff5939dde063dd5d2ea14f6fdcf4a3f19ba01
Andrew Gough <andrew@goughy.org>
2012-12-04 16:15:44 +1100

    Make HTTP headers multi-valued

--- http4d/examples/luasp/lsp_standalone.d
+++ http4d/examples/luasp/lsp_standalone.d
@@ -5,11 +5,11 @@ int main( string [] args )
 {
     LspState state = new LspState( new class LspCallback 
     {
-        void writer( in string content )                    { write( content );           }
-        void log( lazy string msg )                         { writefln( "LOG: %s", msg ); }
-        void error( in string msg )                         { writefln( "\nERR: %s", msg );       }
-        string getHeader( in string name )                  { return "";                  }
-        void setHeader( in string name, in string value )   { /*ignored*/                 }
+        void writer( in string content )                    { write( content );             }
+        void log( lazy string msg )                         { writefln( "LOG: %s", msg );   }
+        void error( in string msg )                         { writefln( "\nERR: %s", msg ); }
+        string[] getHeader( in string name )                { return [""];                  }
+        void setHeader( in string name, in string value )   { /*ignored*/                   }
     } );
 
     state.process( args[ 1 ] );
